3ea499c35c34532f37faa2c584b94a08d7152e1c
kamalpatel22 <patelkamal2598@gmail.com>
2022-09-20 12:28:46 -0400

    Add project files.

---
+++ Assignment_1/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Employee
+    {
+        private string name;
+        private int number;
+        private Decimal rate;
+        private Double hours;
+        private Decimal gross;
+
+        public Employee(String name, int number, Decimal rate, Double hours)
+        {
+            this.name = name;
+            this.number = number;
+            this.rate = rate;
+            this.hours = hours;
+        }
+
+        public Decimal GetGross()
+        {
+            return gross;
+        }
+
+        public Double GetHours()
+        {
+            return hours;
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+
+        public int GetNumber()
+        {
+            return number;
+        }
+
+        public Decimal GetRate()
+        {
+            return rate;
+        }
+
+        public void SetHours(Double hours) { this.hours = hours; }
+        public void SetName(String name) { this.name = name; }
+        public void SetNumber(int number) { this.number = number; }
+        public void SetRate(Decimal rate) { this.rate = rate; }
+
+        public override string ToString()
+        {
+            return GetName() + ", " + GetGross() + ", " + GetHours() + " , " + GetHashCode() + " , " + GetNumber() + ". ";
+        }
+    }
+}
---
+++ Assignment_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Program
+    {
+        const string FILE = "employees.txt";
+        static void Main(string[] args)
+        {
+            Employee[] employee = new Employee[100];
+            int numberofEmployee = 0;
+            String[] input;
+
+            //try
+            //{
+            StreamReader employeereader = new StreamReader(FILE); 
+                while (!employeereader.EndOfStream)
+            {
+                input = employeereader.ReadLine().Split(',');
+                employee[numberofEmployee] = new Employee(input[0], Int32.Parse(input[1]), Decimal.Parse(input[2]), Double.Parse(input[3]));
+                numberofEmployee++;
+            }
+            employeereader.Close();
+
+            for (int i = 0; i < numberofEmployee; i++)
+            {
+                Console.WriteLine(employee[i]);
+            }
+            //  }
+            // catch(IOException ex)
+            // {
+
+            // Console.WriteLine("Your file name doesn't match!. ");
+            // }
+
+            Console.WriteLine("Enter your choice: ");
+            String option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Console.WriteLine(employee[i]);
+                    }
+                    break;
+            }
+
+        }
+
+    }
+}
